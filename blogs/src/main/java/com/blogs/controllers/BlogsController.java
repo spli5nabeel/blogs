d5e005a60be9eed6e43a1c2cfe64a5c7516e3d84
Nabeel <spli5nabeel@gmail.com>
2023-01-04 16:48:09 +0000

    added pass of parameter

--- blogs/src/main/java/com/blogs/controllers/BlogsController.java
+++ blogs/src/main/java/com/blogs/controllers/BlogsController.java
@@ -3,7 +3,9 @@ package com.blogs.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +14,11 @@ public class BlogsController {
 	@RequestMapping("/names")
 	public List<String> retrieveNames(){
 		return Arrays.asList("A","B","C");
+	}
+	
+	
+	@RequestMapping("/hosts")
+	public String hosts(@RequestParam(name="ip", defaultValue = "0.0.0.0") String ip) {
+		return ip;
 	}
 }
